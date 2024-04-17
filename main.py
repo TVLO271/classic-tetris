afa8aa94a074e6d5d39fcf73448de0209d387fe7
Samanthajulian <sjulian4@students.kennesaw.edu>
2024-04-17 19:07:52 -0400

    Game loop
    Made a simple game loop including the time and color. Imported system for game window to run properly.

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-import pygame
+import pygame,sys
 
 pygame.init()
+
+#create screen color
+dark_blue = (44, 44, 127)
 
 #create game window
 screen = pygame.display.set_mode((300,600))
 pygame.display.set_caption("Classic Tetris")
 
 clock = pygame.time.Clock()
+
+#adding game loop for consistency in all players
+while True: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit
+            sys.exit()
+            
+    #Drawing
+    screen.fill(dark_blue)
+    pygame.display.update()
+    clock.tick(60)
